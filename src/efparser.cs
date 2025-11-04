fcba4caf854fa4edf1fb812328e361c0940fbd23
foffe <foffedriving@gmail.com>
2025-11-04 10:33:55 +0100

    rem

--- src/efparser.cs
+++ src/efparser.cs
@@ -139,15 +139,10 @@ public class EFSodInfo
         var ef = new EFSodInfo();
         var reader = new AsnReader(bytes, AsnEncodingRules.DER);
 
-        //var appTag = new Asn1Tag(TagClass.Application, 23); // Tag 77 for EF.SOD
-
-
         AsnReader outerSeq;
-        //var peekTag = reader.PeekTag();
 
         var peekTag = reader.PeekTag();
-        //Log.Info("Peektag: " + peekTag);
-        //  Log.Info("appTag: " + appTag);
+      
 
         // Be able to switch tags depending on old/new passport
         // new -> app tag 23
@@ -298,124 +293,124 @@ public static class TLVParser
     }
 
     // DG1
-    public class ImplEfDg1TD1 : IEfParser<Dg1Info>
-    {
-        public string Name()
-        {
-            return "DG1";
-        }
+    // public class ImplEfDg1TD1 : IEfParser<Dg1Info>
+    // {
+    //     public string Name()
+    //     {
+    //         return "DG1";
+    //     }
 
-        public Dg1Info ParseFromBytes(byte[] bytes)
-        {
-            var ef = new Dg1Info();
-            var reader = new ByteReader(bytes);
-            int tag = reader.ReadInt(1);
-            if (tag != 0x61) throw new Exception($"Unexpected tag: 0x{tag:X2}");
+    //     public Dg1Info ParseFromBytes(byte[] bytes)
+    //     {
+    //         var ef = new Dg1Info();
+    //         var reader = new ByteReader(bytes);
+    //         int tag = reader.ReadInt(1);
+    //         if (tag != 0x61) throw new Exception($"Unexpected tag: 0x{tag:X2}");
 
-            int length = reader.ReadLength();
-            int innerTag = reader.ReadInt(2);
-            if (innerTag != 0x5F1F) throw new Exception($"Wrong inner tag: 0x{innerTag:X2}");
+    //         int length = reader.ReadLength();
+    //         int innerTag = reader.ReadInt(2);
+    //         if (innerTag != 0x5F1F) throw new Exception($"Wrong inner tag: 0x{innerTag:X2}");
 
-            ef.DocumentCode = reader.ReadInt(2);
-            ef.State = reader.ReadInt(3);
-            ef.DocumentNumber.AddRange(reader.ReadBytes(14));
+    //         ef.DocumentCode = reader.ReadInt(2);
+    //         ef.State = reader.ReadInt(3);
+    //         ef.DocumentNumber.AddRange(reader.ReadBytes(14));
 
-            int isExtended = reader.ReadInt(1);
-            if (isExtended == 1) ef.DocumentNumber.AddRange(reader.ReadBytes(15));
-            else reader.PaddingNext(15);
+    //         int isExtended = reader.ReadInt(1);
+    //         if (isExtended == 1) ef.DocumentNumber.AddRange(reader.ReadBytes(15));
+    //         else reader.PaddingNext(15);
 
-            ef.DateOfBirth = reader.ReadBytes(6);
-            int checkBirth = reader.ReadInt(1);
-            ef.Gender = reader.ReadInt(1);
-            ef.DateExpire = reader.ReadBytes(6);
-            int checkExpire = reader.ReadInt(1);
-            ef.Nationality = reader.ReadBytes(3);
+    //         ef.DateOfBirth = reader.ReadBytes(6);
+    //         int checkBirth = reader.ReadInt(1);
+    //         ef.Gender = reader.ReadInt(1);
+    //         ef.DateExpire = reader.ReadBytes(6);
+    //         int checkExpire = reader.ReadInt(1);
+    //         ef.Nationality = reader.ReadBytes(3);
 
-            reader.PaddingNext(12);
-            ef.Name = reader.ReadString(30);
+    //         reader.PaddingNext(12);
+    //         ef.Name = reader.ReadString(30);
 
-            return ef;
-        }
-    }
-
-
+    //         return ef;
+    //     }
+    // }
 
 
 
-    // DG11
-
-    public static void ParseDG11(byte[] dg11)
-    {
-        var tlvs = TLVParser.Parse(dg11);
-
-        foreach (var tlv in tlvs)
-        {
-            switch (tlv.Tag)
-            {
-                case 0x5F0E: // Optional Details
-                    Console.WriteLine("Place of Birth: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
-                    break;
-                case 0x5F10: // Gender
-                    Console.WriteLine("Gender: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
-                    break;
-                case 0x5F11: // Date of Birth
-                    Console.WriteLine("Date of Birth: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
-                    break;
-                default:
-                    Console.WriteLine($"Unknown DG11 Tag {tlv.Tag:X2}");
-                    break;
-            }
-        }
-    }
 
 
-    // Dg14
-    public static void ParseDG14(byte[] dg14)
-    {
-        var tlvs = TLVParser.Parse(dg14);
+    // // DG11
 
-        foreach (var tlv in tlvs)
-        {
-            if (tlv.Tag == 0x6E) // SecurityInfos
-            {
-                var innerTlvs = TLVParser.Parse(tlv.Value);
-                foreach (var inner in innerTlvs)
-                {
-                    if (inner.Tag == 0x06) // OID
-                    {
-                        string oid = string.Join(".", inner.Value.Select(b => b.ToString()));
-                        Console.WriteLine("Security Feature OID: " + oid);
-                    }
-                }
-            }
-        }
-    }
+    // public static void ParseDG11(byte[] dg11)
+    // {
+    //     var tlvs = TLVParser.Parse(dg11);
+
+    //     foreach (var tlv in tlvs)
+    //     {
+    //         switch (tlv.Tag)
+    //         {
+    //             case 0x5F0E: // Optional Details
+    //                 Console.WriteLine("Place of Birth: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
+    //             case 0x5F10: // Gender
+    //                 Console.WriteLine("Gender: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
+    //             case 0x5F11: // Date of Birth
+    //                 Console.WriteLine("Date of Birth: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
+    //             default:
+    //                 Console.WriteLine($"Unknown DG11 Tag {tlv.Tag:X2}");
+    //                 break;
+    //         }
+    //     }
+    // }
 
 
-    // DG16
-    public static void ParseDG16(byte[] dg16)
-    {
-        var tlvs = TLVParser.Parse(dg16);
+    // // Dg14
+    // public static void ParseDG14(byte[] dg14)
+    // {
+    //     var tlvs = TLVParser.Parse(dg14);
 
-        foreach (var tlv in tlvs)
-        {
-            switch (tlv.Tag)
-            {
-                case 0x5F20: // Contact Person Name
-                    Console.WriteLine("Contact Name: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
-                    break;
-                case 0x5F21: // Contact Address
-                    Console.WriteLine("Contact Address: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
-                    break;
-                case 0x5F22: // Contact Phone
-                    Console.WriteLine("Contact Phone: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
-                    break;
-                default:
-                    Console.WriteLine($"Unknown DG16 Tag {tlv.Tag:X2}");
-                    break;
-            }
-        }
-    }
+    //     foreach (var tlv in tlvs)
+    //     {
+    //         if (tlv.Tag == 0x6E) // SecurityInfos
+    //         {
+    //             var innerTlvs = TLVParser.Parse(tlv.Value);
+    //             foreach (var inner in innerTlvs)
+    //             {
+    //                 if (inner.Tag == 0x06) // OID
+    //                 {
+    //                     string oid = string.Join(".", inner.Value.Select(b => b.ToString()));
+    //                     Console.WriteLine("Security Feature OID: " + oid);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+
+    // // DG16
+    // public static void ParseDG16(byte[] dg16)
+    // {
+    //     var tlvs = TLVParser.Parse(dg16);
+
+    //     foreach (var tlv in tlvs)
+    //     {
+    //         switch (tlv.Tag)
+    //         {
+    //             case 0x5F20: // Contact Person Name
+    //                 Console.WriteLine("Contact Name: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
+    //             case 0x5F21: // Contact Address
+    //                 Console.WriteLine("Contact Address: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
+    //             case 0x5F22: // Contact Phone
+    //                 Console.WriteLine("Contact Phone: " + System.Text.Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
+    //             default:
+    //                 Console.WriteLine($"Unknown DG16 Tag {tlv.Tag:X2}");
+    //                 break;
+    //         }
+    //     }
+    // }
 
     // DG12 Utfärdande myndighet VIKTIG för passiveAuthTest.cs
 
@@ -428,42 +423,37 @@ public static class TLVParser
 
 
     }
-    public static Dg12Info ParseDG12(byte[] dg12)
-    {
-        var tlvs = TLVParser.Parse(dg12);
-        var info = new Dg12Info();
+    // public static Dg12Info ParseDG12(byte[] dg12)
+    // {
+    //     var tlvs = TLVParser.Parse(dg12);
+    //     var info = new Dg12Info();
 
-        foreach (var tlv in tlvs)
-        {
-            switch (tlv.Tag)
-            {
-                case 0x5F0B: // Issuing Authority
-                    Console.WriteLine("Issuing Authority: " + Encoding.UTF8.GetString(tlv.Value));
-                    break;
+    //     foreach (var tlv in tlvs)
+    //     {
+    //         switch (tlv.Tag)
+    //         {
+    //             case 0x5F0B: // Issuing Authority
+    //                 Console.WriteLine("Issuing Authority: " + Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
 
-                case 0x5F0C: // Issuing State or Organisation
-                    Console.WriteLine("Issuing State/Org: " + Encoding.UTF8.GetString(tlv.Value));
-                    break;
+    //             case 0x5F0C: // Issuing State or Organisation
+    //                 Console.WriteLine("Issuing State/Org: " + Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
 
-                case 0x5F0D: // Endorsements / Observations
-                    Console.WriteLine("Endorsements: " + Encoding.UTF8.GetString(tlv.Value));
-                    break;
+    //             case 0x5F0D: // Endorsements / Observations
+    //                 Console.WriteLine("Endorsements: " + Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
 
-                case 0x5F0E: // Other details
-                    Console.WriteLine("Other Details: " + Encoding.UTF8.GetString(tlv.Value));
-                    break;
+    //             case 0x5F0E: // Other details
+    //                 Console.WriteLine("Other Details: " + Encoding.UTF8.GetString(tlv.Value));
+    //                 break;
 
-                default:
-                    Console.WriteLine($"Unknown DG12 Tag {tlv.Tag:X2}");
-                    break;
+    //             default:
+    //                 Console.WriteLine($"Unknown DG12 Tag {tlv.Tag:X2}");
+    //                 break;
 
-            }
-        }
-        return info;
-    }
+    //         }
+    //     }
+    //     return info;
+    // }
 }
-
-
-
-
-// DG13 Ytterligare dokumentdetaljer ex dokumentnummer, typ av dokument, utfärdande plats. Detta kan vara överflödigt men om tid finns över kan detta implementeras
